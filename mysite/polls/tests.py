63105383951df311ab68c182d29b3444e80e5214
Vivek Pabani <vivek.pabani@nexedi.com>
2015-10-21 18:16:56 +0200

    testcases begin.

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
 
+import datetime
+from django.utils import timezone
+
+from .models import Question
+
+
 # Create your tests here.
+
+
+class QuestionMethodTests(TestCase):
+ 
+    def test_was_published_recently_with_future_question(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date = time)
+        self.assertEqual(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        time = timezone.now() - datetime.timedelta(days=30)
+        future_question = Question(pub_date = time)
+        self.assertEqual(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        time = timezone.now() - datetime.timedelta(hours=1)
+        future_question = Question(pub_date = time)
+        self.assertEqual(future_question.was_published_recently(), True)
+
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Question, Choice
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.views import generic
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list
-    }
-    return render(request, 'polls/index.html',context)
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+ 
 
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
 def vote(request, question_id):
@@ -33,6 +33,7 @@ def vote(request, question_id):
         return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
